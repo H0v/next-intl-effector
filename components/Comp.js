bf6932ba2d9d24a7b48d82ebfccdf9b5aa44e6ae
Hovhannes <tonoyan6666@gmail.com>
2020-11-02 16:33:07 +0400

    move translating to _app

--- components/Comp.js
+++ components/Comp.js
@@ -1,47 +1,43 @@
 import { useState } from "react";
-import { IntlProvider } from "react-intl";
+
 import { FormattedMessage } from "react-intl";
-import en from "../public/static/locales/en/translate.json";
-import ru from "../public/static/locales/ru/translate.json";
 
 function Comp() {
-  const [language, setLanguage] = useState({
-    locale: "en",
-    messages: en,
-  });
+  //   const [language, setLanguage] = useState({
+  //     locale: "en",
+  //     messages: en,
+  //   });
   return (
     <>
-      <IntlProvider locale={language.locale} messages={language.messages}>
-        <main>
-          <FormattedMessage
-            id="title"
-            // defaultMessage="Learn React"
-            // description="Link on react page"
-            values={{ test: "posmotrim blyaty" }}
-          />
+      <main>
+        <FormattedMessage
+          id="title"
+          // defaultMessage="Learn React"
+          // description="Link on react page"
+          values={{ test: "posmotrim blyaty" }}
+        />
 
-          <button
-            onClick={() =>
-              setLanguage({
-                locale: "ru",
-                messages: ru,
-              })
-            }
-          >
-            Rus
-          </button>
-          <button
-            onClick={() =>
-              setLanguage({
-                locale: "en",
-                messages: en,
-              })
-            }
-          >
-            Eng
-          </button>
-        </main>
-      </IntlProvider>
+        <button
+          onClick={() =>
+            setLanguage({
+              locale: "ru",
+              messages: ru,
+            })
+          }
+        >
+          Rus
+        </button>
+        <button
+          onClick={() =>
+            setLanguage({
+              locale: "en",
+              messages: en,
+            })
+          }
+        >
+          Eng
+        </button>
+      </main>
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import "../styles/globals.css";
+import { IntlProvider } from "react-intl";
+import en from "../public/static/locales/en/translate.json";
+import ru from "../public/static/locales/ru/translate.json";
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <IntlProvider locale="en" messages={en}>
+      <Component {...pageProps} />
+    </IntlProvider>
+  );
 }
 
 export default MyApp;
